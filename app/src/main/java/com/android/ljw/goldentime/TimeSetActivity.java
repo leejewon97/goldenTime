3a847302c36891339ad6dd99788fac07f9493586
ELISE\leeje <leejewon97@naver.com>
2019-04-14 20:57:57 +0900

    v21 삭제(버튼 안보이는 오류 해결), numberPicker 설정

--- app/src/main/java/com/android/ljw/goldentime/TimeSetActivity.java
+++ app/src/main/java/com/android/ljw/goldentime/TimeSetActivity.java
@@ -2,6 +2,7 @@ package com.android.ljw.goldentime;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
 public class TimeSetActivity extends AppCompatActivity
 {
@@ -10,5 +11,49 @@ public class TimeSetActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_set);
+
+        NumberPicker hourPicker = findViewById(R.id.hours);
+        NumberPicker minutePicker = findViewById(R.id.minutes);
+
+        hourPicker.setMinValue(1);
+        hourPicker.setMaxValue(100);
+        hourPicker.setWrapSelectorWheel(false);
+        hourPicker.setOnLongPressUpdateInterval(100);
+        minutePicker.setMinValue(0);
+        minutePicker.setMaxValue(59);
+        minutePicker.setOnLongPressUpdateInterval(100);
+
+//        final Calendar calendar = Calendar.getInstance();
+//
+//        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                TimePickerDialog dialog = new TimePickerDialog(TimeSetActivity.this, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
+//
+//                        String msg = String.format("%d 시 %d 분", hour, min);
+//                        Toast.makeText(TimeSetActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                    }
+//                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);  //마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
+//                dialog.show();
+//            }
+//        });
+//        findViewById(R.id.btn_end).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                TimePickerDialog dialog = new TimePickerDialog(TimeSetActivity.this, new TimePickerDialog.OnTimeSetListener() {
+//                    @Override
+//                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
+//
+//                        String msg = String.format("%d 시 %d 분", hour, min);
+//                        Toast.makeText(TimeSetActivity.this, msg, Toast.LENGTH_SHORT).show();
+//                    }
+//                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);  //마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
+//                dialog.show();
+//            }
+//        });
     }
 }
