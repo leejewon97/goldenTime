6e77a26eaf67feee7468594c001adb11ad946c30
ELISE\leeje <leejewon97@naver.com>
2019-04-16 23:27:12 +0900

    TimePicker Dialog 임시적용

--- app/src/main/java/com/android/ljw/goldentime/TimeSetActivity.java
+++ app/src/main/java/com/android/ljw/goldentime/TimeSetActivity.java
@@ -1,8 +1,14 @@
 package com.android.ljw.goldentime;
 
+import android.app.TimePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class TimeSetActivity extends AppCompatActivity
 {
@@ -23,37 +29,37 @@ public class TimeSetActivity extends AppCompatActivity
         minutePicker.setMaxValue(59);
         minutePicker.setOnLongPressUpdateInterval(100);
 
-//        final Calendar calendar = Calendar.getInstance();
-//
-//        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                TimePickerDialog dialog = new TimePickerDialog(TimeSetActivity.this, new TimePickerDialog.OnTimeSetListener() {
-//                    @Override
-//                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
-//
-//                        String msg = String.format("%d 시 %d 분", hour, min);
-//                        Toast.makeText(TimeSetActivity.this, msg, Toast.LENGTH_SHORT).show();
-//                    }
-//                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);  //마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
-//                dialog.show();
-//            }
-//        });
-//        findViewById(R.id.btn_end).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//
-//                TimePickerDialog dialog = new TimePickerDialog(TimeSetActivity.this, new TimePickerDialog.OnTimeSetListener() {
-//                    @Override
-//                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
-//
-//                        String msg = String.format("%d 시 %d 분", hour, min);
-//                        Toast.makeText(TimeSetActivity.this, msg, Toast.LENGTH_SHORT).show();
-//                    }
-//                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);  //마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
-//                dialog.show();
-//            }
-//        });
+        final Calendar calendar = Calendar.getInstance();
+
+        findViewById(R.id.btn_start).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimePickerDialog dialog = new TimePickerDialog(TimeSetActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
+
+                        String msg = String.format("%d 시 %d 분", hour, min);
+                        Toast.makeText(TimeSetActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);  //마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
+                dialog.show();
+            }
+        });
+        findViewById(R.id.btn_end).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                TimePickerDialog dialog = new TimePickerDialog(TimeSetActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int hour, int min) {
+
+                        String msg = String.format("%d 시 %d 분", hour, min);
+                        Toast.makeText(TimeSetActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false);  //마지막 boolean 값은 시간을 24시간으로 보일지 아닐지
+                dialog.show();
+            }
+        });
     }
 }
