d3aebfd65aba6750538b82da9e70b69623725fe5
ELISE\leeje <leejewon97@naver.com>
2019-06-13 13:07:09 +0900

    DataBinding 및 btn_minus 처리

--- app/src/main/java/com/android/ljw/goldentime/NumSetActivity.java
+++ app/src/main/java/com/android/ljw/goldentime/NumSetActivity.java
@@ -1,33 +1,51 @@
 package com.android.ljw.goldentime;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.android.ljw.goldentime.databinding.ActivityNumSetBinding;
+
 public class NumSetActivity extends AppCompatActivity
 {
-    EditText editText1, editText2, editText3, editText4, editText5;
-    Button btn_plus1, btn_plus2, btn_plus3, btn_plus4, btn_plus5;
-    Button btn_minus1, btn_minus2, btn_minus3, btn_minus4, btn_minus5;
-    Button btn_set;
+    ActivityNumSetBinding numSetBinding;
+    Button[] minus;
+    EditText[] num;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_num_set);
+        numSetBinding = DataBindingUtil.setContentView(this, R.layout.activity_num_set);
 
-        btn_set = findViewById(R.id.btn_set);
+        minus = new Button[]{numSetBinding.btnMinus1, numSetBinding.btnMinus2, numSetBinding.btnMinus3, numSetBinding.btnMinus4, numSetBinding.btnMinus5};
+        num = new EditText[]{numSetBinding.editText1, numSetBinding.editText2, numSetBinding.editText3, numSetBinding.editText4, numSetBinding.editText5};
 
-        btn_set.setOnClickListener(new View.OnClickListener()
+        numSetBinding.btnSet.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
+        for (int i = 0; i < 5; i++) {
+            minus[i].setOnClickListener(btnListener);
+        }
     }
+
+    private View.OnClickListener btnListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view) {
+            for (int i = 0; i < 5; i++){
+                if (view.getId() == minus[i].getId()){
+                    delete(num[i]);
+                }
+            }
+        }
+    };
 
     private void delete(EditText editText) {
         editText.setText("");
