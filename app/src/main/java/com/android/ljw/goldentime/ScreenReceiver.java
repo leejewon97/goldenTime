591dc0f5441607adbd7c8f252bfff2b575a5555e
ELISE\leeje <leejewon97@naver.com>
2019-06-25 01:34:14 +0900

    sendSMS할때 예외처리 하는걸로 옮김

--- app/src/main/java/com/android/ljw/goldentime/ScreenReceiver.java
+++ app/src/main/java/com/android/ljw/goldentime/ScreenReceiver.java
@@ -9,8 +9,6 @@ import android.content.SharedPreferences;
 import android.util.Log;
 import android.widget.Toast;
 
-import java.text.Format;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static android.content.Context.MODE_PRIVATE;
@@ -19,13 +17,10 @@ public class ScreenReceiver extends BroadcastReceiver
 {
     private SharedPreferences timeData;
     Boolean setCheck = false;
-    Boolean notSendCheck = false;
     AlarmManager alarmManager;
     PendingIntent pIntent;
     Calendar calendar;
-    long dates[] = new long[6];
-    int eDates[][][] = new int[5][2][3];
-    int start_eHour, start_eMin, end_eHour, end_eMin;
+    static long[] dates = new long[6];
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -44,11 +39,7 @@ public class ScreenReceiver extends BroadcastReceiver
             Toast.makeText(context, "Screen Off", Toast.LENGTH_SHORT).show();
             // 화면이 꺼지면, 알람을 켬
             setAlarm(context, getTime());
-//            if (TimeSetActivity.exceptCheck) {
-//                eLoad();
-//                selectDates();
-//                plusTime();
-//            }
+
             setCheck = true;
         }
         if (intent.getAction().equals(".intent_gogo")) {
@@ -67,67 +58,6 @@ public class ScreenReceiver extends BroadcastReceiver
         }
     }
 
-    private int plusTime() {
-        return 0;
-    }
-
-    private void selectDates() {
-        SimpleDateFormat yFormat = new SimpleDateFormat("yy");
-        SimpleDateFormat MFormat = new SimpleDateFormat("MM");
-        SimpleDateFormat dFormat = new SimpleDateFormat("dd");
-        SimpleDateFormat[] formats = {yFormat, MFormat, dFormat};
-
-        if (start_eHour < end_eHour || (start_eHour == end_eHour && start_eMin < end_eMin)) {
-            // 시작시각 < 끝시각
-            for (int i = 0; i < eDates.length; i++) {
-                for (int j = 0; j < eDates[i].length; j++) {
-                    for (int k = 0; k < eDates[i][j].length; k++) {
-                        eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i]));
-                    }
-                }
-            }
-        } else if (start_eHour > end_eHour || (start_eHour == end_eHour && start_eMin > end_eMin)) {
-            // 시작시각 > 끝시각
-            int set_hour, set_min;
-            SimpleDateFormat HFormat = new SimpleDateFormat("HH");
-            SimpleDateFormat mFormat = new SimpleDateFormat("mm");
-            set_hour = Integer.parseInt(HFormat.format(dates[0]));
-            set_min = Integer.parseInt(mFormat.format(dates[0]));
-
-            if (set_hour < end_eHour || (set_hour == end_eHour && set_min <= end_eMin)) {
-                // set시각 <= 끝시각
-                for (int i = 0; i < eDates.length; i++) {
-                    for (int j = 0; j < eDates[i].length; j++) {
-                        for (int k = 0; k < eDates[i][j].length; k++) {
-                            if (j == 0) {
-                                if (i == 0) {
-                                    // dates[-1] 처리 (-1일)
-                                    eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[0] - (24 * 60 * 60)));
-                                } else
-                                    eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i - 1]));
-                            } else if (j == 1) {
-                                eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i]));
-                            }
-                        }
-                    }
-                }
-            } else {
-                for (int i = 0; i < eDates.length; i++) {
-                    for (int j = 0; j < eDates[i].length; j++) {
-                        for (int k = 0; k < eDates[i][j].length; k++) {
-                            if (j == 0) {
-                                eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i]));
-                            } else if (j == 1) {
-                                eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i + 1]));
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-
     private void setAlarm(Context context, long time) {
         Log.e("testAL", "setAlarm()");
         calendar = Calendar.getInstance();
@@ -139,14 +69,24 @@ public class ScreenReceiver extends BroadcastReceiver
 //        for (int i = 0; i < 6; i++) {
 //            Log.e("날짜", "dates[" + i + "] : " + sdformat.format(dates[i]));
 //        }
+
 //        SimpleDateFormat yFormat = new SimpleDateFormat("yy");
 //        SimpleDateFormat MFormat = new SimpleDateFormat("MM");
 //        SimpleDateFormat dFormat = new SimpleDateFormat("dd");
+//        SimpleDateFormat[] formats = {yFormat, MFormat, dFormat};
 //        for (int i = 0; i < eDates.length; i++) {
 //            for (int j = 0; j < eDates[i].length; j++) {
-//                eDates[i][j][0] = Integer.parseInt(yFormat.format(dates[i]));
-//                eDates[i][j][1] = Integer.parseInt(MFormat.format(dates[i]));
-//                eDates[i][j][2] = Integer.parseInt(dFormat.format(dates[i]));
+//                for (int k = 0; k < eDates[i][j].length; k++) {
+//                    if (j == 0) {
+//                        if (i == 0) {
+//                            // dates[-1] 처리 (-1일)
+//                            eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[0] - (24 * 60 * 60 * 1000)));
+//                        } else
+//                            eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i - 1]));
+//                    } else if (j == 1) {
+//                        eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i]));
+//                    }
+//                }
 //            }
 //        }
 //        for (int i = 0; i < eDates.length; i++) {
@@ -157,38 +97,31 @@ public class ScreenReceiver extends BroadcastReceiver
 //            }
 //        }
 
-        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+            alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
-        Intent intent = new Intent(context, SendSms.class);
-        pIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            Intent intent = new Intent(context, SendSms.class);
+            pIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + time, pIntent);
+            alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + time, pIntent);
+        }
+
+        private void releaseAlarm (Context context){
+            Log.e("testAL", "releaseAlarm()");
+            alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+
+            Intent Intent = new Intent(context, SendSms.class);
+            pIntent = PendingIntent.getActivity(context, 0, Intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            alarmManager.cancel(pIntent);
+        }
+
+        private long getTime () {
+            int hour = timeData.getInt("HOUR", 1);
+            int min = timeData.getInt("MIN", 0);
+
+            long time = (hour * 3600 + min * 60) * 1000 / 3 * 3;
+            Log.e("testAL", hour + "시 " + min + "분");
+
+            return time;
+        }
     }
-
-    private void releaseAlarm(Context context) {
-        Log.e("testAL", "releaseAlarm()");
-        alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-
-        Intent Intent = new Intent(context, SendSms.class);
-        pIntent = PendingIntent.getActivity(context, 0, Intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        alarmManager.cancel(pIntent);
-    }
-
-    private long getTime() {
-        int hour = timeData.getInt("HOUR", 1);
-        int min = timeData.getInt("MIN", 0);
-
-        long time = (hour * 3600 + min * 60) * 1000 / 3 * 3;
-        Log.e("testAL", hour + "시 " + min + "분");
-
-        return time;
-    }
-
-    private void eLoad() {
-        start_eHour = timeData.getInt("START_E_HOUR", 0);
-        start_eMin = timeData.getInt("START_E_MIN", 0);
-        end_eHour = timeData.getInt("END_E_HOUR", 0);
-        end_eMin = timeData.getInt("END_E_MIN", 0);
-    }
-}
--- app/src/main/java/com/android/ljw/goldentime/SendSms.java
+++ app/src/main/java/com/android/ljw/goldentime/SendSms.java
@@ -1,16 +1,112 @@
 package com.android.ljw.goldentime;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.text.SimpleDateFormat;
+
 public class SendSms extends AppCompatActivity
 {
+    int start_eHour, start_eMin, end_eHour, end_eMin;
+    private SharedPreferences timeData;
+    int eDates[][][] = new int[5][2][3];
+    long dates[] = ScreenReceiver.dates;
+    Boolean notSendCheck = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_send_sms);
         Log.e("testal", "send sms");
+        timeData = getSharedPreferences("timeData", MODE_PRIVATE);
+
+        if (TimeSetActivity.exceptCheck) {
+            eLoad();
+            selectDates();
+//            send 할때 체크
+//            long 추가시간
+//            for (예외날들.length) {
+//                추가시간 += 예외(예외날들[i][0], 예외날들[i][1])
+//
+//            send 할때 체크
+//            If(예외확인 = true) {
+//                sendSMS 대신 setAlarm(추가시간)
+//                예외확인 = false;
+//            }else
+//                sendSms()
+        } else
+            sendSms();
+    }
+
+    private void sendSms() {
+    }
+
+    private int except() {
+        return 0;
+    }
+
+    private void eLoad() {
+        start_eHour = timeData.getInt("START_E_HOUR", 0);
+        start_eMin = timeData.getInt("START_E_MIN", 0);
+        end_eHour = timeData.getInt("END_E_HOUR", 0);
+        end_eMin = timeData.getInt("END_E_MIN", 0);
+    }
+
+    private void selectDates() {
+        SimpleDateFormat yFormat = new SimpleDateFormat("yy");
+        SimpleDateFormat MFormat = new SimpleDateFormat("MM");
+        SimpleDateFormat dFormat = new SimpleDateFormat("dd");
+        SimpleDateFormat[] formats = {yFormat, MFormat, dFormat};
+
+        if (start_eHour < end_eHour || (start_eHour == end_eHour && start_eMin < end_eMin)) {
+            // 시작시각 < 끝시각
+            for (int i = 0; i < eDates.length; i++) {
+                for (int j = 0; j < eDates[i].length; j++) {
+                    for (int k = 0; k < eDates[i][j].length; k++) {
+                        eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i]));
+                    }
+                }
+            }
+        } else if (start_eHour > end_eHour || (start_eHour == end_eHour && start_eMin > end_eMin)) {
+            // 시작시각 > 끝시각
+            int set_hour, set_min;
+            SimpleDateFormat HFormat = new SimpleDateFormat("HH");
+            SimpleDateFormat mFormat = new SimpleDateFormat("mm");
+            set_hour = Integer.parseInt(HFormat.format(dates[0]));
+            set_min = Integer.parseInt(mFormat.format(dates[0]));
+
+            if (set_hour < end_eHour || (set_hour == end_eHour && set_min <= end_eMin)) {
+                // set시각 <= 끝시각
+                for (int i = 0; i < eDates.length; i++) {
+                    for (int j = 0; j < eDates[i].length; j++) {
+                        for (int k = 0; k < eDates[i][j].length; k++) {
+                            if (j == 0) {
+                                if (i == 0) {
+                                    // dates[-1] 처리 (-1일)
+                                    eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[0] - (24 * 60 * 60 * 1000)));
+                                } else
+                                    eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i - 1]));
+                            } else if (j == 1) {
+                                eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i]));
+                            }
+                        }
+                    }
+                }
+            } else {
+                for (int i = 0; i < eDates.length; i++) {
+                    for (int j = 0; j < eDates[i].length; j++) {
+                        for (int k = 0; k < eDates[i][j].length; k++) {
+                            if (j == 0) {
+                                eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i]));
+                            } else if (j == 1) {
+                                eDates[i][j][k] = Integer.parseInt(formats[k].format(dates[i + 1]));
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
