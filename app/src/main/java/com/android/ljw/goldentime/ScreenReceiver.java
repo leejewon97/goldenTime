59cb26aa401fb060d3a91eda6bee6d3a0ce8ac4f
ELISE\leeje <leejewon97@naver.com>
2019-07-01 22:40:32 +0900

    after restart app, sensor in background 실패 - PhoneStateListener 등록

--- app/src/main/java/com/android/ljw/goldentime/ScreenReceiver.java
+++ app/src/main/java/com/android/ljw/goldentime/ScreenReceiver.java
@@ -6,96 +6,43 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.util.Log;
-import android.widget.Toast;
 
 import static android.content.Context.MODE_PRIVATE;
-import static android.content.Context.SENSOR_SERVICE;
+import static android.content.Context.TELEPHONY_SERVICE;
 
 public class ScreenReceiver extends BroadcastReceiver
 {
-    private static final int SENSE_THRESHOLD = 200;
-    long lastTime;
-    float lastX, lastY, lastZ;
     private SharedPreferences timeData;
-    static Boolean checkMoVE = true, checkSet = false;
+    Boolean checkRinged = false;
+    static Boolean checkSet = false;
     static AlarmManager alarmManager;
     static PendingIntent pIntent;
     static long[] dates = new long[6];
-    SensorManager sensorManager;
-    SensorEventListener listener;
 
     @Override
-    public void onReceive(final Context context, Intent intent) {
+    public void onReceive(Context context, Intent intent) {
         Log.e("testsc", "onReceive");
         timeData = context.getSharedPreferences("timeData", MODE_PRIVATE);
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(TELEPHONY_SERVICE);
 
         if (intent.getAction().equals(Intent.ACTION_SCREEN_ON) && checkSet) {
             Log.e("testsc", "Screen On");
-            Toast.makeText(context, "Screen On", Toast.LENGTH_SHORT).show();
 
-            sensorManager = (SensorManager) context.getSystemService(SENSOR_SERVICE);
+            telephonyManager.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
 
-            listener = new SensorEventListener()
-            {
-                @Override
-                public void onSensorChanged(SensorEvent sensorEvent) {
-                    Log.e("롸", "롸?????");
-                    if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-                        long currTime = System.currentTimeMillis();
-                        long gabOfTime = currTime - lastTime;
-
-                        // 0.1초보다 오래되면 다음을 수행 (100ms)
-                        if (gabOfTime > 100) {
-                            lastTime = currTime;
-
-                            float currX = sensorEvent.values[0];
-                            float currY = sensorEvent.values[1];
-                            float currZ = sensorEvent.values[2];
-
-                            // 변위의 절대값에  / lGabOfTime * 10000 하여 스피드 계산
-                            float speed = Math.abs(currX + currY + currZ - lastX - lastY - lastZ) / gabOfTime * 10000;
-                            Log.e("speed", "" + speed);
-
-                            // 임계값보다 크게 움직였을 경우 다음을 수행
-                            if (speed > SENSE_THRESHOLD) {
-                                Log.e("sensor", "sensor sense");
-                                Log.e("speed", "" + speed);
-                                releaseAlarm(context);
-                            } else {
-                                checkMoVE = false;
-                            }
-
-                            // 마지막 위치 저장
-                            // m_fLastX = event.values[0]; 그냥 배열의 0번 인덱스가 X값
-                            lastX = sensorEvent.values[0];
-                            lastY = sensorEvent.values[1];
-                            lastZ = sensorEvent.values[2];
-                        }
-                    }
-                }
-
-                @Override
-                public void onAccuracyChanged(Sensor sensor, int i) {
-
-                }
-            };
-            sensorManager.registerListener(listener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
-
-            //화면이 켜지면, 알람을 끔
-//            releaseAlarm(context);
+            if (telephonyManager.getCallState() == TelephonyManager.CALL_STATE_RINGING)
+                checkRinged = true;
+            else
+                releaseAlarm(context);
         }
         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
             Log.e("testsc", "Screen Off");
-            if (!checkMoVE) {
-                sensorManager.unregisterListener(listener);
-                Log.e("testsc", "Sensor Off");
-            } else {
-                // 화면이 꺼지면, 알람을 켬
+            telephonyManager.listen(listener, PhoneStateListener.LISTEN_NONE);
+
+            if (!checkRinged) {
                 setAlarm(context, getTime());
                 if (timeData.getBoolean("EXCEPT_CHECK", false)) {
                     Intent service_intent = new Intent(context, CalculateETService.class);
@@ -108,7 +55,8 @@ public class ScreenReceiver extends BroadcastReceiver
                         context.startService(service_intent);
                     }
                 }
-            }
+            } else
+                checkRinged = false;
         }
         if (intent.getAction().equals(".intent_gogo")) {
             Log.e("testsc", "intent gogo");
@@ -158,9 +106,6 @@ public class ScreenReceiver extends BroadcastReceiver
         }
 
         alarmManager.cancel(pIntent);
-        sensorManager.unregisterListener(listener);
-        Log.e("testsc", "Sensor Off");
-        checkMoVE = true;
         checkSet = false;
     }
 
@@ -172,4 +117,14 @@ public class ScreenReceiver extends BroadcastReceiver
 
         return time;
     }
+
+    PhoneStateListener listener = new PhoneStateListener()
+    {
+        public void onCallStateChanged(int state, String incomingNumber) {
+            if (state == TelephonyManager.CALL_STATE_OFFHOOK) {
+                checkRinged = false;
+                Log.e("call", "받음");
+            }
+        }
+    };
 }
