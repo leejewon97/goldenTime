468107df95baad1f87b22a10149051506a911373
ELISE\leeje <leejewon97@naver.com>
2019-07-02 23:35:36 +0900

    SOS 생성 및 구현(send sms service intent에 state를 추가해서 구분), widget 생성

---
+++ app/src/main/java/com/android/ljw/goldentime/SosSetActivity.java
@@ -0,0 +1,78 @@
+package com.android.ljw.goldentime;
+
+import android.content.SharedPreferences;
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.android.ljw.goldentime.databinding.ActivitySosSetBinding;
+
+public class SosSetActivity extends AppCompatActivity
+{
+    private SharedPreferences numData;
+    ActivitySosSetBinding sosSetBinding;
+    Button[] nums;
+    String[] key;
+    String sosNum;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        sosSetBinding = DataBindingUtil.setContentView(this, R.layout.activity_sos_set);
+
+        nums = new Button[]{sosSetBinding.button, sosSetBinding.button2, sosSetBinding.button3, sosSetBinding.button4, sosSetBinding.button5};
+        key = new String[]{"TEXT1", "TEXT2", "TEXT3", "TEXT4", "TEXT5"};
+
+        numData = getSharedPreferences("numData", MODE_PRIVATE);
+        sosNum = numData.getString("SOS", "");
+
+        load();
+
+        for (int i = 0; i < 5; i++) {
+            nums[i].setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View view) {
+                    Log.e("click", "onClick");
+                    sosNum = numData.getString("SOS", "");
+                    SharedPreferences.Editor editor = numData.edit();
+
+                    for (int j = 0; j < 5; j++) {
+                        if (view.getId() == nums[j].getId()) {
+                            if (sosNum.equals("")) {
+                                for (int k = 0; k < 5; k++) {
+                                    nums[k].setEnabled(false);
+                                }
+                                nums[j].setEnabled(true);
+                                editor.putString("SOS", nums[j].getText().toString());
+                            } else {
+                                for (int k = 0; k < 5; k++) {
+                                    nums[k].setEnabled(true);
+                                }
+                                editor.remove("SOS");
+                            }
+                            editor.apply();
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private void load() {
+        for (int i = 0; i < 5; i++)
+            nums[i].setText(numData.getString(key[i], ""));
+        if (!sosNum.equals("")) {
+            for (int i = 0; i < 5; i++) {
+                nums[i].setEnabled(false);
+                if (nums[i].getText().equals(sosNum)) {
+                    nums[i].setEnabled(true);
+                }
+            }
+        }
+    }
+}
